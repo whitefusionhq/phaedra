6beb5ac4ef118e025c118328029613b23fc0cafe
Jared White <jared@jaredwhite.com>
2020-06-23 23:42:12 -0700

    test GH webhook

--- example/api/env.rb
+++ example/api/env.rb
@@ -4,7 +4,7 @@ class PhaedraFunction < Phaedra::Base
   def get(params)
     response["Content-Type"] = "text/html; charset=utf-8"
     <<~HTML
-      <p>Hello! 😃</p>
+      <p>Hello friend! 😃</p>
       <p>Startup Time: #{Phaedra::Shared.the_time}</p>
       <p>Environment: #{Phaedra.environment}</p>
       <p>Current Time: #{Time.new}</p>
